5d53737f0303fdf707c12c69d067973e14dbfadc
CrazyWZbiel2010 <bieldonato781@gmail.com>
2026-08-07 16:42:24 -0300

    atualizei o projeto

---
+++ script.js
@@ -0,0 +1,11 @@
+var email = document.getElementById("Email")
+var senha = document.getElementById("senha")
+
+
+function entrar(){
+    if(email.value == "gabrieldonato303@gmail.com" && email.value == "Blizz1705"){
+        alert("Entrando!")
+    }else{
+        alert(email.value)
+    }
+}
