1da16ac15aa8547275c4a4244c5d392950fbaef6
CrazyWZbiel2010 <bieldonato781@gmail.com>
2026-08-10 11:04:02 -0300

    atualizei o site

---
+++ scripts/script.js
@@ -0,0 +1,18 @@
+var email = document.getElementById("Email")
+var senha = document.getElementById("senha")
+
+
+document.addEventListener('keydown', function(event) {
+    if (event.key === 'Enter') {
+        entrar()
+    }
+})
+
+function entrar(){
+    if(email.value == "gabrieldonato303@gmail.com" && senha.value == "Blizz1705"){
+        window.location.href="../abas/home.html";
+    }else{
+        alert("Email ou senha invalidos")
+    }
+}
+
